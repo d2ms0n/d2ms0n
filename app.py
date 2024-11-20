91b0a3c31cf959cf75f18b6797708597d8375391
Дмитрий <d2ms0n@gmail.com>
2024-11-20 14:12:35 +0500

    Добавлено CORS

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import datetime
 from typing import Annotated, Optional
 from fastapi import FastAPI, Form
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 class Order(BaseModel):
     number : int
@@ -54,6 +55,13 @@ repo = [
 
 
 app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins = ["*"],
+  allow_methods = ["*"],
+  allow_headers = ["*"]
+)
 
 message = ""
  
